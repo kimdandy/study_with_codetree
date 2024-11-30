94b30069b39dd07a89318d6ba099d4a5a54d6cbe
kimdandy <d22psource@gmail.com>
2024-11-30 12:36:55 +0900

    Title: 단어 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241130/단어 출력/print-word.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // cout 호출에 필요한 헤더
+using namespace std; // 매번 std::을 cout앞에 붙일 필요가 없어짐
+
+int main() {
+    // std::cout << "Hello"; // 2번줄로 std:: 생략 가능
+    cout << "Hello"; // cout : 출력함수
+    return 0; 
+}
