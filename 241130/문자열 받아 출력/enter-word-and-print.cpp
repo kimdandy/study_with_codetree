a4d823d3c752d7806608d86a023c6fb8f36dc057
kimdandy <d22psource@gmail.com>
2024-11-30 14:46:28 +0900

    Title: 문자열 받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241130/문자열 받아 출력/enter-word-and-print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string> // string 호출에 필요한 헤더
+using namespace std;
+
+int main() {
+    // 문자열(단어) 입력 ; string
+    string s;
+    cin >> s;
+    cout << s;
+    return 0;
+}
