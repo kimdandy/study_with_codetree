46493022acf46e3051a6e1c5e8983d7704b0b6c8
kimdandy <d22psource@gmail.com>
2024-11-30 14:04:05 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241130/실수 받아 그대로 출력/enter-real-value-and-print.cpp
+++ 241130/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -6,7 +6,7 @@ int main() {
     double n;
     cout << fixed; //소수점 고정
     cin >> n;
-    cout.precision(2); // 소수점 2째 자리 까지 반올림
+    cout.precision(2); // 소수점 2번째 자리까지 반올림
     cout << n;
 
     return 0;
