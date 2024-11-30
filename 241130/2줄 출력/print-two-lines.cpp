a29c8e413db01cf2e1a28635b66cc5aa52771ae2
kimdandy <d22psource@gmail.com>
2024-11-30 12:41:48 +0900

    Title: 2줄 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241130/2줄 출력/print-two-lines.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello";
+    // 줄바꿈
+    //1
+    cout << endl; 
+    //2
+    //cout << "\n";
+    cout << "World";
+    return 0;
+}
