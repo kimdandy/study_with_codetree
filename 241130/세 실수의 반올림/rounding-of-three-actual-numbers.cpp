d65a4daabcdb0492e21bb4a99982a8675bf95c9d
kimdandy <d22psource@gmail.com>
2024-11-30 14:34:31 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241130/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
+++ 241130/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     double a, b,c;
     cout << fixed;
-    cin >> a >> b >> c;
+    cin >> a >> b >> c; // 1.1278\n85.21344\n577.999564 입력
     cout.precision(3);
     cout << a << endl << b << "\n" << c;
     return 0;
