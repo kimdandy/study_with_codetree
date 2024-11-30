2dc1f58ae4279dbc7936144e91418c4088bfac3a
kimdandy <d22psource@gmail.com>
2024-11-30 14:23:36 +0900

    Title: 입력받아 계산 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241130/입력받아 계산 3/input-calculate-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 2개의 줄에 걸쳐 입력
+    // ; cin은 줄바꿈 명령어 \n도 입력에서 무시함
+    int a, b;
+    cin >> a >> b; 
+    /*34 
+      63 입력*/ 
+    int c= a*b; 
+    cout << c; 
+    return 0;
+}
