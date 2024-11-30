22287e3ec96d3f5319509664c987e8562cb2f271
kimdandy <d22psource@gmail.com>
2024-11-30 13:46:18 +0900

    Title: 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241130/입력받아 계산/input-calculate.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // cout과 마찬가지로 cin 호출에 필요한 헤더
+using namespace std; // std:: 생략
+
+int main() {
+    // 입력 : cin >>
+    int a;
+    cin >> a; 
+    int aa = a+2;
+    cout << aa;
+    return 0;
+}
