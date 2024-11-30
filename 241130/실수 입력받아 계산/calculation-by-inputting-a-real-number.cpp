5ce748410a1ccc11612028421c91e771b5b90020
kimdandy <d22psource@gmail.com>
2024-11-30 14:30:49 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241130/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
+++ 241130/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     double a, b;
     cout << fixed;
-    cin >> a >> b; // 5.26\n3.14
+    cin >> a >> b; // 5.26\n3.14 입력
     cout.precision(2);
     cout << a+b;
 
