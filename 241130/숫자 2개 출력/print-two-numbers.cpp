29ce484d84fa9d1d46fb3924a8dfb9687c69eec0
kimdandy <d22psource@gmail.com>
2024-11-30 12:44:15 +0900

    Title: 숫자 2개 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241130/숫자 2개 출력/print-two-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 공백을 사이에 두고 출력
+    // 1 - " " 삽입
+    cout << 3 << " " << 5; 
+    // 2 - 문장이라면 그냥 바로 공백 삽입
+    //cout << "3 5";
+    return 0;
+}
