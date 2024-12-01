4701eb66528a7416e9c4782f22c8095e31621350
kimdandy <d22psource@gmail.com>
2024-12-01 17:28:25 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241201/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int u, d;
+    char h;
+    // 1.
+    cin >> u >> h >> d;
+
+    // 2. cin.get()
+    /*cin >> u;
+    cin.get();
+    cin >> d;*/
+
+    cout << u << d;
+
+    return 0;
+}
