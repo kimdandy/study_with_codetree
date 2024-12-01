64db11f39dfc3142379f4e4308584a2b62b4992c
kimdandy <d22psource@gmail.com>
2024-12-01 18:00:06 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241201/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int m = a*b, d= a/b;
+    cout << a << " * " << b << " = " << m << endl << a << " / " << b << " = " << d;
+    return 0;
+}
