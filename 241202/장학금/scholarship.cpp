cb56d2121b4a00feccacaa5c2e6d5af24780db13
kimdandy <d22psource@gmail.com>
2024-12-02 20:17:57 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241202/장학금/scholarship.cpp
+++ 241202/장학금/scholarship.cpp
@@ -8,10 +8,15 @@ using namespace std;
 int main() {
     int mid, fin;
     cin >> mid >> fin;
-
+/*
     if (mid < 90) cout << 0;
     else if (fin >= 95 ) cout << 100000;
     else if (fin >= 90) cout << 50000;
+    else cout << 0;
+*/
+
+    if (mid>=90 and fin >=95 ) cout << 100000;
+    else if (mid >=90 and fin >=90) cout << 50000;
     else cout << 0;
 
     return 0;
