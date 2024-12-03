c059f22898377f4724b46b54933db4512450dd66
kimdandy <d22psource@gmail.com>
2024-12-03 21:20:21 +0900

    Title: 문자열 출력하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241203/문자열 출력하기/print-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/// Q. 정수 n의 값을 입력받고 그 값의 횟수만큼 LeebrosCode를 출력하는 프로그램을 작성해보세요.
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++)
+        cout << "LeebrosCode" << endl;
+        
+    return 0;
+}
