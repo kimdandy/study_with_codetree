2e2d2d1e351db8543678545507d374523304f4b4
kimdandy <d22psource@gmail.com>
2024-12-03 19:59:14 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241203/중앙값 구하기/find-the-median.cpp
+++ 241203/중앙값 구하기/find-the-median.cpp
@@ -11,17 +11,17 @@ int main() {
     cin >> a >> b >> c;
 
     if (a>=b){
-        if (b>=c) cout << b;
+        if (b>=c) cout << b; // a>b>c 므로 중앙값 b
         else {
-            if (a>=c) cout << c;
-            else cout << a;
+            if (a>=c) cout << c; // a>c>b 므로 중앙값 c
+            else cout << a; // c>a>b므로 중앙값 a
         }
     }
     else {
-        if (a>=c) cout << a;
+        if (a>=c) cout << a; // b>a>c므로 중앙값 a
         else {
-            if (b>=c) cout << c;
-            else cout << b;
+            if (b>=c) cout << c; // b>c>a므로 중앙값 c
+            else cout << b; // c>b>a므로 중앙값 b
         }
     }
     return 0;
