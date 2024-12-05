160b9c12ac803949a96438e06740cd1ee35e31fa
kimdandy <d22psource@gmail.com>
2024-12-05 19:47:14 +0900

    Title: a부터 b까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241205/a부터 b까지의 합/sum-from-a-to-b.cpp
+++ 241205/a부터 b까지의 합/sum-from-a-to-b.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 /// Q. a, b 두 자연수를 입력받아 a부터 b까지의 합을 구해 출력하는 프로그램을 작성해보세요.
 int main() {
-    int a, b, sum=0;
+    int a, b, sum=0; // sum값 초기화
     cin >> a >> b;
 
     for(int i=a;i<=b;i++) sum+=i;
