2e6c5675c43471073be5aac5f9dd9d25d0721679
kimdandy <d22psource@gmail.com>
2024-12-05 21:52:42 +0900

    Title: 20대의 평균 나이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241205/20대의 평균 나이/average-age-of-20.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/// Q. 몇 명의 사람들의 나이가 차례대로 주어질 때, 처음으로 20대가 아닌 다른 나이대의 사람이 나오기 전까지 입력된 나이들의 평균을 구하는 프로그램을 작성해보세요.
+// 첫 번째 사람은 반드시 20대라고 가정해도 좋습니다.
+int main() {
+    int age; // 1<=age<=100
+    int sum=0, cnt=0;
+    while(3){
+        cin >> age;
+        if(age/10 != 2) break;
+        sum+=age;
+        cnt+=1;
+    }
+    cout << fixed;
+    cout.precision(2);
+    cout << double(sum)/cnt;
+    return 0;
+}
