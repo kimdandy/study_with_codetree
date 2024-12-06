4664ba94197df0f987f68443b8ac4fd2fb68c2df
kimdandy <d22psource@gmail.com>
2024-12-07 00:26:51 +0900

    Title: 별표 출력하기 12, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241207/별표 출력하기 12/print-star-12.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+/* Q. 정수 n이 주어질 때, 아래 입출력 예제와 같이 별표를 출력하는 프로그램을 작성해보세요.
+예제1
+입력:
+1
+출력:
+*
+
+예제2
+입력:
+2
+출력:
+* *
+  *
+
+예제3
+입력:
+3
+출력:
+* * *
+  *
+
+예제4
+입력:
+4
+출력:
+* * * *
+  *   *
+      *
+      *
+
+예제5
+입력:
+5
+출력:
+* * * * *
+  *   *
+      *
+      *
+
+*/
+int main() {
+    int n; // 1<=n<=15
+    cin >> n;
+
+    
+    for(int i=1; i<=n; i++){
+        if(i==1){
+            for(int j=1;j<=n;j++) cout << "* ";
+        }
+        else {  
+            for(int j=1;j<=(i-1)/2;j++) cout << "    ";
+            if(n%2==1)
+              for(int j=(n-i+1)/2;j>=1;j--) cout << "  * ";
+            else
+              for(int j=(n-i)/2+1;j>=1;j--) cout << "  * ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
