2a812107f64939e8272a0104c8b85c0852fd6471
kimdandy <d22psource@gmail.com>
2024-12-09 21:25:07 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241209/소수 구하기/get-prime.cpp
+++ 241209/소수 구하기/get-prime.cpp
@@ -12,5 +12,18 @@ int main() {
         for(int j=1;j<=i;j++) if(i%j==0) cnt ++;
         if(cnt==2) cout << i << " ";
     }
+
+    /*// 해설
+    for(int i = 1; i <= n; i++) {
+		if(i == 1) continue;
+		bool isprime = true;
+		
+		for(int j = 2; j < i; j++)
+			if(i % j == 0) isprime = false;
+		
+		if(isprime)
+			cout << i << " ";
+	}
+    */
     return 0;
 }
