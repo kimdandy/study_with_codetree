f470b33818efbe456e332ad1ffbeb20f9cdc4a93
kimdandy <d22psource@gmail.com>
2024-12-09 21:23:26 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241209/소수 구하기/get-prime.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/// Q. 1이상 n이하의 소수를 오름차순으로 출력하는 프로그램을 작성해보세요.
+// 각각 공백을 사이에 두고 출력
+int main() {
+    int n; // 1<=n<=100
+    cin >> n;
+
+    for(int i=1;i<=n;i++){
+        int cnt =0 ;
+        for(int j=1;j<=i;j++) if(i%j==0) cnt ++;
+        if(cnt==2) cout << i << " ";
+    }
+    return 0;
+}
