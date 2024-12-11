30de06c46ebcb04cc0bea039f8dbcd366351648a
kimdandy <d22psource@gmail.com>
2024-12-11 21:32:36 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241211/연속부분수열일까/contiguous-array-or-not.cpp
+++ 241211/연속부분수열일까/contiguous-array-or-not.cpp
@@ -15,14 +15,15 @@ int main() {
     for(int idx2=0;idx2<n2;idx2++) cin >> b[idx2]; // 1~100
     //cout << n1 << " " << n2 << endl;
 
-    int cur2 = 0; // 2번 비교 위치(0~n2)
-    bool isright = false; // 여부
-    for(int idx1=0; idx1<n1; idx1++){
+    int cur2 = 0; // 2번 비교 위치(0~n2) , 첫원소(b[0]) 부터 비교
+    bool isright = false; // 포함 여부
+    for(int idx1=0; idx1<n1; idx1++){ 
         if(a[idx1]==b[cur2] && (cur2==0 || a[idx1-1]==b[cur2-1]) ) cur2++;
-        else cur2=0;
-        if(cur2==n2){
-            isright = true;
-            break;
+        // 두 원소값이 일치할 때 비교위치가 0이거나 이전 위치의 두 원소도 일치한다면 다음 원소도 비교(비교위치 1 증가)
+        else cur2=0; // 다르다면 비교위치 초기화
+        if(cur2==n2){ // 비교위치값이 b배열의 크기(n2)와 같아지면
+            isright = true; // 포함 확인하고
+            break; // 루프 종료
         }
     }
     if(isright == true) cout << "Yes";
