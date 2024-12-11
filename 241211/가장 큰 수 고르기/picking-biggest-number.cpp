56382ac8c8f3cc0fc44e1914f22b45e9bce667e4
kimdandy <d22psource@gmail.com>
2024-12-11 21:43:38 +0900

    Title: 가장 큰 수 고르기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241211/가장 큰 수 고르기/picking-biggest-number.cpp
+++ 241211/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 //// 주어진 숫자들 중 최댓값 구하기
+// 지금까지 구한 최댓값인 max_val이라는 값보다 현재 값인 elem이 더 큰 경우라면, 최댓값을 갱신
 
 ///  Q. 10개의 정수가 주어지면 그 중 가장 큰 수를 골라 출력하는 프로그램을 작성하세요.
 int main() {
