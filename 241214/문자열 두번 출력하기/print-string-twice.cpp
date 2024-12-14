6bed2e3a43e17baa08bd978b69f56633b544a0db
kimdandy <d22psource@gmail.com>
2024-12-14 21:06:05 +0900

    Title: 문자열 두번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241214/문자열 두번 출력하기/print-string-twice.cpp
+++ 241214/문자열 두번 출력하기/print-string-twice.cpp
@@ -25,10 +25,11 @@ cout << str[2];  // n < -문자열의 세 번째 문자
 */
 
 /// Q. 문자열이 주어지면 2번에 걸쳐 해당 문자열을 출력하는 프로그램을 작성해보세요.
+// 첫 번째 줄에 길이가 100 이하인 문자열이 주어집니다.
 #include <string> // 문자열 함수 string을 호출하기 위해 필요한 헤더
 
 int main() {
-    string str; 
+    string str; // 문자열의 크기는 일단 신경쓰지 않는다
     cin >> str;
     for(int i=0;i<2;i++) cout << str << endl;
     return 0;
