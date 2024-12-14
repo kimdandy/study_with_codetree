d392dacc7c30416f31d21d29c04b72d91b6de471
kimdandy <d22psource@gmail.com>
2024-12-14 22:41:14 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241214/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/// Q. 알파벳으로 이루어져 있고 공백을 포함한 10개의 문자열이 주어지면 공백으로 구분하여 단어로 나누어 출력하는 프로그램을 작성해보세요.
+#include <string>
+int main() {
+    string str[10];
+    for(int i=0;i<10;i++){
+        cin >> str[i]; // strlen ≤ 200
+        cout << str[i] << endl;
+    }
+    return 0;
+}
