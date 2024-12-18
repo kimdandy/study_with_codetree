9f39441f7eaa4e126d35103b0aafc6d4e08884e3
kimdandy <d22psource@gmail.com>
2024-12-18 20:36:27 +0900

    Title: 각 자리 숫자들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241218/각 자리 숫자들의 합/sum-of-each-digit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/// Q. 정수 n이 주어지면 각 자리 숫자들의 합을 구해 출력하는 프로그램을 작성해보세요.
+#include <string>
+int main() {
+    int n; // 1 ≤ n ≤ 10,000
+    cin >> n;
+
+    string sn = to_string(n);
+
+    int sum=0;
+    for(int i=0;i<sn.length();i++) sum+= (int)(sn[i] - '0');
+
+    cout << sum;
+    
+    return 0;
+}
